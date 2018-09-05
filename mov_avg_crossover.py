2e66de08b7294de11c5b6739506d81b6a0107772
Manoj Shenoy <manoj.shenoy@gmail.com>
2018-09-05 13:00:38 -0400

    Find out which EMA optimizes the cumulative return

--- mov_avg_crossover.py
+++ mov_avg_crossover.py
@@ -6,60 +6,67 @@ import matplotlib.pyplot as plt
 import csv; import datetime; import pytz
 from technical_indicators import BBANDS
 from historical_data import exchange_data,write_to_csv,to_unix_time
-import backtest
+# import backtest
 
 # ==========Initial trade parameters =============
 symbol = 'BTC/USD'
-timeframe = '1d'
+timeframe = '5m'
 trading_qty = 1.0
-since = '2018-01-01 00:00:00'
+since = '2017-01-01 00:00:00'
 hist_start_date = int(to_unix_time(since))
 header = ['Timestamp', 'Open', 'High', 'Low', 'Close', 'Volume']
-periods = 89
+period = []
 
 # ==========Initial exchange parameters =============
-kraken = exchange_data('kraken','BTC/USD',timeframe=timeframe,since=hist_start_date)
+kraken = exchange_data('kraken', 'BTC/USD', timeframe=timeframe, since=hist_start_date)
 write_to_csv(kraken,'BTC/USD','kraken')
 
-data = pd.DataFrame(kraken,columns=header)
+data = pd.DataFrame(kraken, columns=header)
 print(data.head())
 
+
+# ======= Find out which EMA optimizes the cumulative return =======
 # ============ Strategy Function - Exponential Moving average crossover ================
-def strategy(data,periods):
-    ema_short = data['Close'].ewm(span=periods, adjust=False).mean()
+def strategy(data):
+    for period in range(20,100,5):
+        ema_short = data['Close'].ewm(span=period, adjust=False).mean()
 
-    # Difference between prices & EMA timeseries
-    trading_positions_raw = data['Close'] - ema_short
-    #trading_positions_raw.tail()
+        # Difference between prices & EMA timeseries
+        trading_positions_raw = data['Close'] - ema_short
+        #trading_positions_raw.tail()
 
-    # Taking the sign of the difference to determine whether the price or EMA is greater and then
-    # multiply by qty
+        # Taking the sign of the difference to determine whether the price or EMA is greater and then
+        # multiply by qty
 
-    trading_positions = trading_positions_raw.apply(np.sign)
-    print('Trading Positions:', trading_positions.head())
+        trading_positions = trading_positions_raw.apply(np.sign)
+        print('Trading Positions:', trading_positions.head())
 
-    # Lagging our trading signals by one period
-    trading_positions_final = trading_positions.shift(1) * trading_qty
+        # Lagging our trading signals by one period
+        trading_positions_final = trading_positions.shift(1) * trading_qty
 
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(16, 9))
+        data['returns'] = np.log(data['Close'].shift(1) / data['Close'])
 
-    ax1.plot(data.loc[since:,:].index,data.loc[since:,'Close'],label = 'Price')
-    ax1.plot(ema_short.loc[since:,:].index, ema_short.loc[since:,'Close'], label = "89 EMA")
-    ax1.legend(loc='best')
-    ax1.set_ylabel('Price($)')
+        data['strat_returns'] = data['returns'] * trading_positions_final
+        cum_returns = data['strat_returns'].dropna().cumsum().apply(np.exp)
 
-    ax2.plot(trading_positions_final.loc[since:,:].index, trading_positions_final[since:,'Close'],
-             label='Trading_Position')
-    ax2.set_ylabel('Trading position')
+        print cum_returns
 
-    plt.show()
+        # fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(16, 9))
+        #
+        # ax1.plot(data.loc[since:, :].index, data.loc[since:, 'Close'], label = 'Price')
+        # # ax1.plot(ema_short.loc[since:, :].index, ema_short.loc[since:, 'Close'], label = "89 EMA")
+        # ax1.legend(loc='best')
+        # ax1.set_ylabel('Price($)')
+        #
+        # ax2.plot(cum_returns.loc[since:, :].index, cum_returns[since:, 'Close'],
+        #          label='Equity Curve')
+        # ax2.set_ylabel('Equity Curve')
+        #
+        # plt.show()
 
-    data['returns'] = np.log(data['Close'].shift(1) / data['Close'])
+    return data['strat_returns'], cum_returns
 
-    data['strat_returns'] = data['returns'] * trading_positions_final
-
-    return data['strat_returns']
-
-returns = strategy(data, periods)
-backtest.drawdown_periods(returns)
-backtest.underwater_plot(returns)
+returns, equity_curve = strategy(data)
+print(returns, equity_curve)
+# backtest.drawdown_periods(returns)
+# backtest.underwater_plot(returns)
