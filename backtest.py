2bc4214100de294eacd3c90cab5e83387ea1bf83
Manoj Shenoy <manoj.shenoy@gmail.com>
2018-09-05 16:08:43 -0400

    ===== Directional Backtesting platform using Pyfolio ======
    Functions for Drawdown Periods, Underwater plots, Monthly & Annual returns.
    Create a comprehensive Tear sheet for strategy

--- backtest.py
+++ backtest.py
@@ -5,11 +5,7 @@ import matplotlib.pyplot as plt
 import pyfolio as pf
 import csv; import datetime; import pytz
 from technical_indicators import BBANDS
-from historical_data import exchange_data,write_to_csv,to_unix_time
-from empyrical.utils import nanmean
-from zipline.utils import math_utils
-
-
+from historical_data import exchange_data, write_to_csv, to_unix_time
 
 symbol = 'BTC/USD'
 timeframe = '1d'
@@ -21,8 +17,13 @@ kraken = exchange_data('kraken','BTC/USD',timeframe=timeframe,since=hist_start_d
 write_to_csv(kraken,'BTC/USD','kraken')
 data = pd.DataFrame(kraken,columns=header)
 
-
-
+# ===== Directional Strategy & Backtesting platform using Pyfolio ======
+# ======================================================================
+'''
+Functions for Drawdown Periods, Underwater plots, Monthly 
+& Annual returns.
+Create a comprehensive Tear sheet for strategy
+'''
 def drawdown_periods(returns):
     fig = plt.figure(facecolor='white')
     plt.yscale('log')
@@ -31,7 +32,7 @@ def drawdown_periods(returns):
 
 def underwater_plot(returns):
     fig = plt.figure(facecolor='white')
-    ax=pf.plot_drawdown_underwater(returns)
+    ax = pf.plot_drawdown_underwater(returns)
     plt.savefig('underwater_plot.png')
 
 def monthly_returns(returns):
@@ -46,31 +47,3 @@ def annual_returns(returns):
 
 def create_full_tear_sheet(returns):
     pf.create_full_tear_sheet(returns)
-
-# =================================================================
-# ================ DIRECTIONAL STRATEGY ===========================
-stddev = 2; n = 20  # ====== parameters
-
-# ==== Calling the function from technical_indicators.py ==========
-bbands = BBANDS(data, stddev, n)
-
-
-def strategy(data):
-    buy = data['Close'] > bbands['BB_Upper20']
-    sell = data['Close'] > bbands['BB_Lower20']
-
-    data['returns'] = np.log(data['Close']/data['Close'].shift(1))
-
-    if buy == True:
-        data['position'] = 1
-    elif sell == True:
-        data['position'] = -1
-
-    data['strat_returns'] = data['position'].shift(1) * data['returns']
-    data['cum_returns'] = data['strat_returns'].dropna().cumsum().apply(np.exp)
-
-    return data['strat_returns']
-
-returns = strategy(data=data)
-drawdown_periods(returns)
-underwater_plot(returns)
